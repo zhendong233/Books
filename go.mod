6632192ca024ea7b9ac1811ccc2bf704eb2ac7a7
zd-zhang <zd_zhang@dreamarts.co.jp>
2020-12-04 17:28:05 +0800

    hello

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zhendong233/Books
 
 go 1.15
+
+require github.com/jmoiron/sqlx v1.2.0
--- internal/book/respository/book_respository.go
+++ internal/book/respository/book_respository.go
@@ -1,4 +1,24 @@
 package respository
 
+import (
+	"context"
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+
+	"github.com/zhendong233/Books/internal/book/model"
+)
+
 type BookRespository interface {
+	FindByID(ctx context.Context, bookID string) (*model.Book, error)
+}
+
+type bookRespository struct {
+	db *sqlx.DB
+}
+
+func NewBookRespository(db *sql.DB) *bookRespository {
+	return &bookRespository{
+		db: sqlx.NewDb(db, "mysql"),
+	}
 }
