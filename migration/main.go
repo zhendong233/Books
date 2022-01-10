27836de364d7d3cd9b607c8e96a255520bef0ed6
zd-zhang <zd_zhang@dreamarts.co.jp>
2022-01-10 17:00:21 +0800

    add migration main

---
+++ migration/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/zhendong233/Books/pkg/dbutil"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Print(err)
+		os.Exit(-1)
+	}
+}
+
+func run() error {
+	db, err := dbutil.NewDB()
+	if err != nil {
+		return err
+	}
+	if err := dbutil.ExecSQLFile(context.Background(), db, "./migration/sql/createTable.sql"); err != nil {
+		return err
+	}
+	return nil
+}
