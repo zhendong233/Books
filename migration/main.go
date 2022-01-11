33799a280793ed8824546df04ae51f0e184a8cdb
zd-zhang <zd_zhang@dreamarts.co.jp>
2022-01-11 12:54:07 +0800

    fix main

--- migration/main.go
+++ migration/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql" // mysql
+
 	"github.com/zhendong233/Books/pkg/dbutil"
+)
+
+const (
+	Conn    = "books:books@tcp(localhost:3309)/books"
+	ConnStr = Conn + `?charset=utf8mb4&parseTime=True&loc=UTC&tls=false&multiStatements=true`
 )
 
 func main() {
@@ -16,13 +24,16 @@ func main() {
 }
 
 func run() error {
-	db, err := dbutil.NewDB()
+	db, err := sql.Open("mysql", ConnStr)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		_ = db.Close()
 	}()
+	db.SetConnMaxLifetime(5)
+	db.SetMaxOpenConns(100)
+	db.SetMaxIdleConns(16)
 	if err := dbutil.ExecSQLFile(context.Background(), db, "./migration/sql/createTable.sql"); err != nil {
 		return err
 	}
