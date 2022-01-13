d2803a1848c7b81cd09abc9e1b2aa9e54456ce4a
zd-zhang <zd_zhang@dreamarts.co.jp>
2022-01-13 08:19:44 +0800

    fix github actions test

---
+++ migration/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql" // mysql
+
+	"github.com/zhendong233/Books/pkg/dbutil"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Print(err)
+		os.Exit(-1)
+	}
+}
+
+func run() error {
+	db, err := dbutil.NewDB()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+	if err := dbutil.ExecSQLFile(context.Background(), db, "./migration/sql/createTable.sql"); err != nil {
+		return err
+	}
+	return nil
+}
