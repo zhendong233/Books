33c1de90fc4d84238d7ce6be2aab4f6666ef7c17
zd-zhang <zd_zhang@dreamarts.co.jp>
2022-01-13 08:26:13 +0800

    not use dbutil package

--- migration/main.go
+++ migration/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"context"
+	"bufio"
+	"database/sql"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql" // mysql
+)
 
-	"github.com/zhendong233/Books/pkg/dbutil"
+const (
+	Conn    = "books:books@tcp(localhost:3309)/books"
+	ConnStr = Conn + `?charset=utf8mb4&parseTime=True&loc=UTC&tls=false&multiStatements=true`
 )
 
 func main() {
@@ -18,15 +24,36 @@ func main() {
 }
 
 func run() error {
-	db, err := dbutil.NewDB()
+	db, err := sql.Open("mysql", ConnStr)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		_ = db.Close()
 	}()
-	if err := dbutil.ExecSQLFile(context.Background(), db, "./migration/sql/createTable.sql"); err != nil {
+	db.SetConnMaxLifetime(5)
+	db.SetMaxOpenConns(100)
+	db.SetMaxIdleConns(16)
+	f, err := os.Open("./migration/sql/createTable.sql")
+	if err != nil {
 		return err
+	}
+	defer f.Close()
+	scan := bufio.NewScanner(f)
+	var sb strings.Builder
+	reg := regexp.MustCompile(`--.*$`)
+	for scan.Scan() {
+		line := scan.Text()
+		q := reg.ReplaceAllString(line, "") // 去掉sql文件中的注释
+		sb.WriteString(q)
+	}
+	qs := strings.Split(sb.String(), ";")
+	for _, q := range qs {
+		if q != "" {
+			if _, err := db.Exec(q); err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }
